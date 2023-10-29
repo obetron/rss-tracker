32caa321a92000d54519b349bab8a4f6f3c961ed
obetron <erenbasaran@gmail.com>
2023-10-29 18:25:50 +0300

    feed repository interface added.

---
+++ src/main/java/com/gelecex/upwork/rsstracker/repository/FeedRepository.java
@@ -0,0 +1,11 @@
+package com.gelecex.upwork.rsstracker.repository;
+
+import com.gelecex.upwork.rsstracker.model.Feed;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author obetron
+ * @date 29.10.2023 - 18:25
+ */
+public interface FeedRepository extends CrudRepository<Feed, String> {
+}
--- src/main/java/com/gelecex/upwork/rsstracker/repository/RssTrackerRepository.java
+++ src/main/java/com/gelecex/upwork/rsstracker/repository/RssTrackerRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  * @author obetron
  * @date 28.10.2023 - 15:18
  */
-
 public interface RssTrackerRepository extends CrudRepository<RssTracker, String>{
 }
